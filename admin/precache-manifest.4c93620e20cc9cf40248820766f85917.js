6768b165f3977465e992a2854086aa1ec2bca7b3
Denglee <1007358560@qq.com>
2020-07-14 00:07:58 +0800

    0714

---
+++ admin/precache-manifest.4c93620e20cc9cf40248820766f85917.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e498c0593d38a5ecd01",
+    "url": "admin/css/Layout.1594655921441.css"
+  },
+  {
+    "revision": "a3465ee7e996f5f69557",
+    "url": "admin/css/app.1594655921441.css"
+  },
+  {
+    "revision": "91be149a7c0df64f02ae",
+    "url": "admin/css/chunk-vendors.1594655921441.css"
+  },
+  {
+    "revision": "842ad759fa2bd49c8ca6",
+    "url": "admin/css/matchIndex.1594655921441.css"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "admin/fonts/element-icons.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "admin/fonts/element-icons.woff"
+  },
+  {
+    "revision": "bdc8eb7d6356a74ea38ed2819e2a2f58",
+    "url": "admin/img/icon_goTop.png"
+  },
+  {
+    "revision": "28d900fae07d2f2529445f26ca3ead75",
+    "url": "admin/img/icon_goTop2.png"
+  },
+  {
+    "revision": "9e498c0593d38a5ecd01",
+    "url": "admin/js/Layout.1594655921441.js"
+  },
+  {
+    "revision": "a3465ee7e996f5f69557",
+    "url": "admin/js/app.1594655921441.js"
+  },
+  {
+    "revision": "849c8d0f0aca0df0c9e0",
+    "url": "admin/js/bankDetails.1594655921441.js"
+  },
+  {
+    "revision": "d7ec28f5fed87881ec69",
+    "url": "admin/js/bankList.1594655921441.js"
+  },
+  {
+    "revision": "91be149a7c0df64f02ae",
+    "url": "admin/js/chunk-vendors.1594655921441.js"
+  },
+  {
+    "revision": "df4504a0dd5fbebc0abe",
+    "url": "admin/js/loanInfo.1594655921441.js"
+  },
+  {
+    "revision": "481fdeee8a4f966ee297",
+    "url": "admin/js/loanItem.1594655921441.js"
+  },
+  {
+    "revision": "842ad759fa2bd49c8ca6",
+    "url": "admin/js/matchIndex.1594655921441.js"
+  },
+  {
+    "revision": "9632d1c244e93392990edfdb205fdd4a",
+    "url": "index.html"
+  },
+  {
+    "revision": "ac7696a8741f147ee94ca4087007049a",
+    "url": "logo.ico"
+  },
+  {
+    "revision": "1577c67267a9ca5a96d6b3f6004d7e1c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
